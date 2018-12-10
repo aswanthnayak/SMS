c89e847a2afd041ac261813b6e7ce97e08baee77
Aswanthnayak <33441756+aswanthnayak@users.noreply.github.com>
2018-12-11 01:44:20 +0530

    Update models.py

--- gate/models.py
+++ gate/models.py
@@ -16,31 +16,22 @@ class user_details(models.Model):
 	entry_created_by=models.CharField(max_length=30,default=None,null=True)
 	entry_last_modified_time=models.DateTimeField(default=datetime.now())
 	entry_last_modified_by=models.CharField(max_length=30,default=None,null=True)
-		
-class log(models.Model):
-	uid = models.CharField(max_length=15,default=None)
+
+class student_attendance(models.Model):
+	uid=models.CharField(max_length=15,default=None)
+	block=models.CharField(max_length=5,default=None)
 	name= models.CharField(max_length=30,default=None)
-	email_id = models.EmailField(max_length=30,default="abc@gmail.com")
-	block=models.CharField(max_length=5,default=None,null=True)
 	room_no  = models.CharField(max_length=5,default=None,null=True)
-	mobile_no=models.CharField(max_length=30,default=None)
-        	
-class worker_details(models.Model):
-	type_of_work = models.CharField(max_length=10,default=None)
-	uid = models.CharField(max_length=15,default=None)
-	name= models.CharField(max_length=30,default=None)
-	mobile_no=models.CharField(max_length=30,default=None)
-	
-	age = models.IntegerField()
+	presence = models.IntegerField(default=0)
 	entry_created_time=models.DateTimeField(default=datetime.now())
 	entry_created_by=models.CharField(max_length=30,default=None,null=True)
 	entry_last_modified_time=models.DateTimeField(default=datetime.now())
 	entry_last_modified_by=models.CharField(max_length=30,default=None,null=True)
 
+	
 class in_out(models.Model):
 	uid=models.CharField(max_length=15,default=None)
-	#u_id=models.CharField(max_length=15,default=None)
-	#did = models.CharField(max_length=10)
+	
 	name= models.CharField(max_length=30,default=None)
 	in_time=models.CharField(max_length=30,default=None)
 	out_time=models.CharField(max_length=30,default='0')
@@ -80,7 +71,7 @@ class student_service_details(models.Model):
 	room_no=models.CharField(max_length=10,default='BH1')
 	date=models.CharField(max_length=15,default=None)
 	tm=models.CharField(max_length=15,default=None)
-	assigned_worker_id=models.CharField(max_length=15,default=None,null=True)
+	assigned_worker_name=models.CharField(max_length=15,default=None,null=True)
 	worker_assigned_time=models.DateTimeField(default=datetime.now())
 	work_completed_time=models.DateTimeField(default=datetime.now())
 	status=models.IntegerField(default=0)
@@ -103,7 +94,7 @@ class laundry_details(models.Model):
 	no_of_leggins=models.IntegerField(default=0)
 	others=models.IntegerField(default=0)
 	total=models.IntegerField(default=0)
-	assigned_worker_id=models.CharField(max_length=15,null=True,default=None)
+	assigned_worker_name=models.CharField(max_length=15,null=True,default=None)
 	status=models.IntegerField(default=0)
 	entry_created_time=models.DateTimeField(default=datetime.now())
 	entry_created_by=models.CharField(max_length=30,default=None,null=True)
@@ -117,7 +108,7 @@ class faculty_service_details(models.Model):
 	room_no=models.CharField(max_length=10,default=None,null=True)
 	date=models.CharField(max_length=15,default=None)
 	tm=models.CharField(max_length=15,default=None)
-	assigned_worker_id=models.CharField(max_length=15,default=None,null=True)
+	assigned_worker_name=models.CharField(max_length=15,default=None,null=True)
 	status=models.IntegerField(default=0)
 	entry_created_time=models.DateTimeField(default=datetime.now())
 	entry_created_by=models.CharField(max_length=30,default=None,null=True)
@@ -131,7 +122,7 @@ class cooking(models.Model):
 	room_no=models.CharField(max_length=10,default=None,null=True)
 	date=models.CharField(max_length=15,default=None)
 	tm=models.CharField(max_length=15,default=None)
-	assigned_worker_id=models.CharField(max_length=15,default=None,null=True)
+	assigned_worker_name=models.CharField(max_length=15,default=None,null=True)
 	status=models.IntegerField(default=0)
 	entry_created_time=models.DateTimeField(default=datetime.now())
 	entry_created_by=models.CharField(max_length=30,default=None,null=True)
@@ -147,7 +138,6 @@ class book_cab_details(models.Model):
 	tm=models.CharField(max_length=15,default=None)
 	block=models.CharField(max_length=5,default=None)
 	status=models.IntegerField(default=0)
-	time_of_depature=models.DateTimeField(default=datetime.now(),null=True)
 	entry_created_time=models.DateTimeField(default=datetime.now())
 	entry_created_by=models.CharField(max_length=30,default=None,null=True)
 	entry_last_modified_time=models.DateTimeField(default=datetime.now())
@@ -183,8 +173,7 @@ class medical_sevices(models.Model):
 	name=models.CharField(max_length=30,default=None)	
 	room_no=models.CharField(max_length=10,default=None,null=True)
 	block=models.CharField(max_length=10,default=None)
-	#suffering_with/from=models.CharField(max_length=30)
-	#severity=models.CharField(max_length=20,default=None)
+	
 	description=models.CharField(max_length=100,default=None)
 	status=models.IntegerField(default=0)
 	entry_created_time=models.DateTimeField(default=datetime.now())
@@ -204,24 +193,5 @@ class complaints(models.Model):
 	entry_last_modified_time=models.DateTimeField(default=datetime.now())
 	entry_last_modified_by=models.CharField(max_length=30,default=None,null=True)
 
-class attendence_tracker(models.Model):
-	uid=models.CharField(max_length=15,default=None)
-	name=models.CharField(max_length=30,default=None)
-	date=models.DateTimeField(default=datetime.now())
-	entry_created_time=models.DateTimeField(default=datetime.now())
-	entry_created_by=models.CharField(max_length=30,default=None,null=True)
-	entry_last_modified_time=models.DateTimeField(default=datetime.now())
-	entry_last_modified_by=models.CharField(max_length=30,default=None,null=True)
-
-class worker_tracker(models.Model):
-	uid=models.CharField(max_length=15,default=None)
-	block=models.CharField(max_length=10,default=None)
-	#date=models.DateTimeField(default=datetime.now())
-	from_time=models.DateTimeField(default=datetime.now())
-	to_time=models.DateTimeField(default=datetime.now())
-	entry_created_time=models.DateTimeField(default=datetime.now())
-	entry_created_by=models.CharField(max_length=30,default=None,null=True)
-	entry_last_modified_time=models.DateTimeField(default=datetime.now())
-	entry_last_modified_by=models.CharField(max_length=30,default=None,null=True)
 
       
