c871cd930988490cd64ad167fdb77b93ce0407b5
Aswanthnayak <33441756+aswanthnayak@users.noreply.github.com>
2018-12-10 12:23:57 +0530

    Update settings.py

--- security/settings.py
+++ security/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':  'application',
         'USER':  'root',
-        'PASSWORD' : '',
+        'PASSWORD' : 'Sms@123456',
         'HOST' : 'localhost',
         'PORT' : '3306',
     }
